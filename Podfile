8d1132620062411e578053afd25abe6175eaf21b
舒超 <shuchao@live.cn>
2016-07-19 20:05:53 +0800

    1.初始化项目

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, ‘8.0’
+
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'SCWeibo' do
+ # 约束框架
+pod 'SnapKit', '~> 0.18.0'
+# 网络
+pod 'AFNetworking'
+# 指示器
+pod 'SVProgressHUD'
+# 网络图片
+pod 'SDWebImage’
+
+end
---
+++ SCWeibo/Classes/View/Main/SCMainViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SCMainViewController.swift
+//  SCWeibo
+//
+//  Created by 云卷云舒丶 on 16/7/19.
+//
+//
+
+import UIKit
+
+class SCMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    /*
+    */
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
